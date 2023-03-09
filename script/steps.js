a34feea6b1edf9783f263957ed12440eb1f4d114
sundayirvine-code <amugumbiirvine@gmail.com>
2023-03-10 02:54:45 +0300

    more changes

--- script/steps.js
+++ script/steps.js
@@ -5,6 +5,17 @@ const tab4 = document.querySelector(".tab4");
 const tab5 = document.querySelector(".tab5");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
+const step1 = document.querySelector(".step1")
+const step2 = document.querySelector(".step2")
+const step3 = document.querySelector(".step3")
+const step4 = document.querySelector(".step4")
+const online_price = document.querySelector(".online_price")
+const storage_price = document.querySelector(".storage_price");
+const custom_price = document.querySelector(".custom_price");
+
+import {isMonthly} from './tabs.js';
+
+step1.style.backgroundColor = "#bde2fd"
 
 let counter = 0;
 
@@ -14,6 +25,13 @@ function showtab(counter) {
     } else {
       prev.style.display = "block";
     }
+
+    if (counter == 3) {
+      next.innerHTML = "Confirm";
+    } else {
+      next.innerHTML = "Next Step";
+    }
+
     if (counter >= 4) {
       next.style.display = "none";
     } else {
@@ -23,6 +41,10 @@ function showtab(counter) {
     switch (counter) {
         case 0:
           tab1.style.display = "grid";
+          step1.style.backgroundColor = "#bde2fd"
+          step2.style.backgroundColor = ""
+          step3.style.backgroundColor = ""
+          step4.style.backgroundColor = ""
           tab2.style.display = "none";
           tab3.style.display = "none";
           tab4.style.display = "none";
@@ -31,6 +53,10 @@ function showtab(counter) {
         case 1:
           tab1.style.display = "none";
           tab2.style.display = "grid";
+          step2.style.backgroundColor = "#bde2fd"
+          step1.style.backgroundColor = ""
+          step3.style.backgroundColor = ""
+          step4.style.backgroundColor = ""
           tab3.style.display = "none";
           tab4.style.display = "none";
           tab5.style.display = "none";
@@ -39,14 +65,32 @@ function showtab(counter) {
           tab1.style.display = "none";
           tab2.style.display = "none";
           tab3.style.display = "grid";
+          step3.style.backgroundColor = "#bde2fd"
+          step1.style.backgroundColor = ""
+          step2.style.backgroundColor = ""
+          step4.style.backgroundColor = ""
           tab4.style.display = "none";
           tab5.style.display = "none";
+          if (!isMonthly){
+            online_price.innerHTML = "+$10/mo";
+            storage_price.innerHTML = "+$20/mo";
+            custom_price.innerHTML = "+$20/mo";
+          }
+          else{
+            online_price.innerHTML = "+$1/mo";
+            storage_price.innerHTML = "+$2/mo";
+            custom_price.innerHTML = "+$2/mo";
+          }
           break;
         case 3:
           tab1.style.display = "none";
           tab2.style.display = "none";
           tab3.style.display = "none";
           tab4.style.display = "grid";
+          step4.style.backgroundColor = "#bde2fd"
+          step1.style.backgroundColor = ""
+          step2.style.backgroundColor = ""
+          step3.style.backgroundColor = ""
           tab5.style.display = "none";
           break;
         case 4:
@@ -61,26 +105,71 @@ function showtab(counter) {
       }
   }
 
+  const errors = document.querySelectorAll(".error ");
+
+  errors.forEach(error=>{
+    error.style.display = "none";
+  })
+
   function validateForm() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
+    const name_ = document.getElementById("name");
+    const email_ = document.getElementById("email");
+    const phone_ = document.getElementById("phone");
+    const name_error = document.querySelector(".name-error");
+    const mail_error = document.querySelector(".mail-error");
+    const phone_error = document.querySelector(".phone-error");
+    
   
     // Validate name
     if (name.trim() === "") {
+      name_error.style.display = "block";
+      name_.style.borderColor = "#d84357";
       return false;
+    }
+    else{
+      name_.style.borderColor = "#dedde1";
+      name_error.style.display = "none";
     }
   
     // Validate email
     const emailRegex = /\S+@\S+\.\S+/;
-    if (!emailRegex.test(email)) {
+    if (email.trim() == ""){
+      mail_error.style.display = "block";
+      mail_error.innerHTML = "This is required"
+      email_.style.borderColor = "#d84357";
       return false;
+    }
+    if (!emailRegex.test(email)) {
+      mail_error.style.display = "block";
+      mail_error.innerHTML = "Invalid Email"
+      email_.style.borderColor = "#d84357";
+      return false;
+    }
+    else{
+      email_.style.borderColor = "#dedde1";
+      mail_error.style.display = "none";
     }
   
     // Validate phone number
     const phoneRegex = /^\d{10}$/;
-    if (!phoneRegex.test(phone)) {
+    if (phone.trim() === ""){
+      phone_error.style.display = "block";
+      phone_error.innerHTML = "This is required"
+      phone_.style.borderColor = "#d84357";
       return false;
+    }
+    if (!phoneRegex.test(phone)) {
+      phone_error.style.display = "block";
+      phone_error.innerHTML = "Invalid Number"
+      phone_.style.borderColor = "#d84357";
+      return false;
+    }
+    else{
+      phone_.style.borderColor = "#dedde1";
+      phone_error.style.display = "none";
     }
   
     // All fields are valid
@@ -93,7 +182,6 @@ next.addEventListener("click", (event)=>{
 
     //ensure data is provided for
     if (!validateForm()) {
-        alert("Please provide valid data in all fields.");
         return;
     }
     else{
--- script/tabs.js
+++ script/tabs.js
@@ -7,7 +7,8 @@ const price_arcade = document.querySelector(".arcade_price");
 const price_pro = document.querySelector(".pro_price");
 const price_advanced = document.querySelector(".advanced_price");
 
-let isMonthly = true; // initial state
+
+export let isMonthly = true; // initial state
 
 //create an on click event on switch_
 switch_.addEventListener("click", function() {
@@ -35,6 +36,21 @@ switch_.addEventListener("click", function() {
     price_arcade.innerHTML = "$90/mo";
     price_advanced.innerHTML = "$120/mo";
     price_pro.innerHTML = "$150/mo";
+
   }
 
+});
+
+
+const checkboxes = document.querySelectorAll('.add-check');
+
+checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    const parentAddOn = this.parentElement;
+    if (this.checked) {
+      parentAddOn.classList.add('checked');
+    } else {
+      parentAddOn.classList.remove('checked');
+    }
+  });
 });
